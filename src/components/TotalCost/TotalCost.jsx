30ac850a4bfb512174c8bd897ae6359bf2a29c20
James Kelley <jkkelley86@gmail.com>
2021-06-11 09:04:32 -0500

    staged

--- src/components/TotalCost/TotalCost.jsx
+++ src/components/TotalCost/TotalCost.jsx
@@ -5,7 +5,22 @@ function TotalCost() {
   const customerOrder = useSelector((store) => store.customerOrder);
 
   const calculateTotalCost = (customerOrder) => {
-    const pizzas = (sum, pizza) => sum + Number(pizza.total);
+    /*
+    This is the callback used by reduce
+    It will be called for each thing in the array.
+    Whatever it returns will be passed to the next call, in the `sum` variable
+    When the reduce is complete, it passes the final sum to the reduce function.
+    The sum of all the things is returned to who called reduce!
+    */
+    const pizzas = (sum, pizza) => sum + Number(pizza.cost);
+
+    /* 
+    Reduce is an array method, like .map()
+    .reduce() executes the callback function once for each element present in the array
+    The 0 is the starting value -- every call to employeeSalary() increases that value
+    by the employees annual salary.
+    */
+   
     const totalCost = customerOrder.reduce(pizzas, 0);
     return totalCost;
   };
